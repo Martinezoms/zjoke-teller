e564ab0967f362f4a3b51dc8fe4c1dfd8e4fcefd
Zoms <martinezoms15@gmail.com>
2022-01-24 14:56:48 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,44 @@
+const button = document.getElementById("button");
+const audioElement = document.getElementById("audio");
+
+// Disable/Enable button
+function toggleButton() {
+  button.disabled = !button.disabled;
+}
+
+// passing the joke into voiceRss APi
+function tellMejoke(joke) {
+  VoiceRSS.speech({
+    key: API_KEY,
+    src: joke,
+    hl: "en-us",
+    v: "Linda",
+    r: 0,
+    c: "mp3",
+    f: "44khz_16bit_stereo",
+    ssml: false
+  });
+}
+
+// Get jokes from joke API
+async function getJokes() {
+  let joke = "";
+  const apiUrl = "https://v2.jokeapi.dev/joke/Any?blacklistFlags=religious,racist,sexist";
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    if (data.setup) {
+      joke = `${data.setup} ... ${data.delivery}`;
+    } else {
+      joke = data.joke;
+    }
+    tellMejoke(joke);
+    toggleButton();
+  } catch (error) {
+    console.log("whoops", error);
+  }
+}
+
+// Event Listeners
+button.addEventListener("click", getJokes);
+audioElement.addEventListener("ended", toggleButton);
